7d01699e9689a542bb14b538c4331e07525afc81
bosa\Bosiljka <bosiljkakostic1@yahoo.com>
2020-10-31 11:01:20 +0100

    minor cosmetic changes, and maybe a little bit slower loading, experimental branch

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
 // code saparation
 const custSlA = {};
 //Dynamically generate a list: helper function
-custSlA.createElement = function(element, attribute, inner) {
-    if (typeof(element) === "undefined") {
-      return false;
-    }
-    if (typeof(inner) === "undefined") {
-      inner = "";
-    }
-    var el = document.createElement(element);
-    if (typeof(attribute) === 'object') {
-      for (var key in attribute) {
-        el.setAttribute(key, attribute[key]);
-      }
-    }
-    if (!Array.isArray(inner)) {
-      inner = [inner];
-    }
-    for (var k = 0; k < inner.length; k++) {
-      if (inner[k].tagName) {
-        el.appendChild(inner[k]);
-      } else {
-        el.appendChild(document.createTextNode(inner[k]));
-      }
-    }
-    return el;
+custSlA.createElement = function (element, attribute, inner) {
+  if (typeof (element) === "undefined") {
+    return false;
   }
-  //The contents of the folder with images have to be provided on the server-side
-  //I will assume that we have a string provided via html data-xxx attribute with filenames off images.
-
-custSlA.createSliders = function() {
+  if (typeof (inner) === "undefined") {
+    inner = "";
+  }
+  var el = document.createElement(element);
+  if (typeof (attribute) === 'object') {
+    for (var key in attribute) {
+      el.setAttribute(key, attribute[key]);
+    }
+  }
+  if (!Array.isArray(inner)) {
+    inner = [inner];
+  }
+  for (var k = 0; k < inner.length; k++) {
+    if (inner[k].tagName) {
+      el.appendChild(inner[k]);
+    } else {
+      el.appendChild(document.createTextNode(inner[k]));
+    }
+  }
+  return el;
+}
+//The contents of the folder with images have to be provided on the server-side
+//I will assume that we have a string provided via html data-xxx attribute with filenames off images.
+// read data attributes
+custSlA.readDataParameters = function () {
+  const masterContainer = document.querySelector('#slider_param');
+  const dir = masterContainer.getAttribute('data-dir');
+  const extension = masterContainer.getAttribute('data-extension');
+  const filenames = masterContainer.getAttribute('data-filenames');
+  const sliderRows = masterContainer.getAttribute('data-slider-rows');
+  const imgInRow = masterContainer.getAttribute('data-img-in-row');
+  custSlA.arrOposites = masterContainer.getAttribute('data-opposites').split(",");
+  custSlA.arrFaster = masterContainer.getAttribute('data-faster').split(",");
+  custSlA.arrSlower = masterContainer.getAttribute('data-slower').split(",");
+  //number of images * imageWidth have to be greater then .slider width,
+  //but slider with will be corrected if it is not case
+  const arrayFilenames = filenames.split(",");
+  return [dir, extension, filenames, sliderRows, imgInRow, arrayFilenames];
+}
+custSlA.createSliders = function (dir, extension, filenames, sliderRows, imgInRow, arrayFilenames) {
   /*
   template that will be created
   <div class="slider">
@@ -44,18 +59,7 @@ custSlA.createSliders = function() {
     </ul>
   </div>
   */
-  // read data attributes
-  const masterContainer = document.querySelector('#slider_param');
-  const dir = masterContainer.getAttribute('data-dir');
-  const extension = masterContainer.getAttribute('data-extension');
-  const filenames = masterContainer.getAttribute('data-filenames');
-  const sliderRows = masterContainer.getAttribute('data-slider-rows');
-  const imgInRow = masterContainer.getAttribute('data-img-in-row');
-  custSlA.arrOposites = masterContainer.getAttribute('data-opposites').split(",");
-  custSlA.arrFaster = masterContainer.getAttribute('data-faster').split(",");
-  custSlA.arrSlower = masterContainer.getAttribute('data-slower').split(",");
-  //number of images * imageWidth have to be greater then .slider width
-  const arrayFilenames = filenames.split(",");
+
   var currDivOuter;
   var currUl;
   var currLi, currInnerDiv, currImgObj, currBntObj;
@@ -79,7 +83,7 @@ custSlA.createSliders = function() {
     while (custSlA.currentColumnInLineIndex < imgInRow) {
       if (custSlA.currentFileIndex > (arrayFilenames.length - 1)) {
         custSlA.currentFileIndex = 0 // start again from 0 (from beginning)
-          // although the parameters should be set on tahat way that all rows have different files
+        // although the parameters should be set on tahat way that all rows have different files
       }
       currImgObj = 'imgObj_' + ind + '_' + custSlA.currentColumnInLineIndex;
       custSlA[currImgObj] = custSlA.createElement('img', {
@@ -107,7 +111,7 @@ custSlA.createSliders = function() {
   });
 }
 
-custSlA.animations = function() {
+custSlA.animations = function () {
   custSlA.rowsArray.forEach(element => {
     custSlA['ul' + element] = $('#ul' + element);
     custSlA.opposite = custSlA.arrOposites[element - 1];
@@ -121,16 +125,16 @@ custSlA.animations = function() {
       });
     }
 
-    // function for animation
-    custSlA['anim' + element] = function(dur) {
+    // function for animation - parameter dur is the duration in milliseconds
+    custSlA['anim' + element] = function (dur) {
       // adjust slider width to less than ul width
       let ul_width = 0;
       let no = 0;
-      $('#ul'+element).children().each(function() {
+      $('#ul' + element).children().each(function () {
         no++;
         ul_width = ul_width + $(this).width();
       });
-      ul_width = parseInt(((no-1) / no )* ul_width);
+      ul_width = parseInt(((no - 1) / no) * ul_width);
       if (ul_width > 100 && $('.slider').width() > ul_width) { // only if slider wider than ul
         $('.slider').css({
           width: ul_width
@@ -148,7 +152,7 @@ custSlA.animations = function() {
       custSlA['ul' + element].animate(custSlA.animation, {
         duration: dur,
         easing: 'linear',
-        complete: function() {
+        complete: function () {
 
           if (custSlA.arrOposites[element - 1] === 'false') {
             //move the first item and put it as last item
@@ -170,11 +174,11 @@ custSlA.animations = function() {
       });
     }
     custSlA['anim' + element](parseInt(custSlA.arrFaster[element - 1]));
-    custSlA['ul' + element].mouseenter(function() {
+    custSlA['ul' + element].mouseenter(function () {
       custSlA['ul' + element].stop(true, false);
       custSlA['anim' + element](parseInt(custSlA.arrSlower[element - 1]));
     });
-    custSlA['ul' + element].mouseleave(function() {
+    custSlA['ul' + element].mouseleave(function () {
       custSlA['ul' + element].stop(true, false);
       custSlA['anim' + element](parseInt(custSlA.arrFaster[element - 1]));
     });
@@ -183,7 +187,17 @@ custSlA.animations = function() {
     // });
   });
 }
-custSlA.createSliders();
-$(document).ready(function() {
+custSlA.createElements = function () {
+  const arrVars = custSlA.readDataParameters();
+  const dir = arrVars[0];
+  const extension = arrVars[1];
+  const filenames = arrVars[2];
+  const sliderRows = arrVars[3];
+  const imgInRow = arrVars[4];
+  const arrayFilenames = arrVars[5];
+  custSlA.createSliders(dir, extension, filenames, sliderRows, imgInRow, arrayFilenames);
+}
+custSlA.createElements();
+$(document).ready(function () {
   custSlA.animations();
 });
